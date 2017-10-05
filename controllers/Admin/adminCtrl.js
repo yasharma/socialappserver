e2d5b91bfbeb4c7bc584a3d949145c7164efc78d
Deepak Kr Yadav <yadav_deepak@seologistics.com>
2017-10-05 17:10:12 +0530

    add forgot password api

--- controllers/Admin/adminCtrl.js
+++ controllers/Admin/adminCtrl.js
@@ -6,6 +6,7 @@ const
 	Setting 	= require(path.resolve('models/Setting')),
 	_ 			= require('lodash'),
 	jwt 	 	= require('jsonwebtoken'),
+	mail 	 	= require(path.resolve('./core/lib/mail')),
   	config 		= require(path.resolve(`./core/env/${process.env.NODE_ENV}`));
 
 exports.register = (req, res, next) => {
@@ -71,6 +72,67 @@ exports.login = (req, res, next) => {
 		}
 	});
 };
+
+exports.forgotpassword= (req,res,next) => {
+
+ if(!req.body.email) {
+	    return res.json({responsedata: {
+	        message: 'Email is required', 
+	        name: 'Authentication failed', 
+	        success: 0} 
+	      });
+	  }
+/*  let email = req.body.email,
+  tmpEmail = _.escapeRegExp(req.body.email);
+
+  async.waterfall([
+    function (done) {
+      User.findOne({ email: {$regex: new RegExp(`^${tmpEmail}`), $options:"im"}}, function (err, user) {
+        if( err ){
+          done(err, null);
+        } else {
+          let responsedata = null, error = false;
+          switch(_.isNull(user) || !_.isNull(user)){
+            case _.isNull(user):
+              responsedata = {name:'Authentication failed', message:'Sorry! We weren\'t able to identify your email',success:0};
+              error = true;
+              break;
+              
+              default: 
+              error = false;  
+          }
+          done(responsedata, user);
+        }
+      });
+    },
+    function (user, done) {
+      
+		mail.send({
+			subject: 'Forgot Password',
+			html: './public/email_templates/user/admin_forgot_password.html',
+			from: config.mail.from, 
+			to: user.email,
+			emailData : {
+	   		    email: user.email,
+	   		    password: user.password
+	   		}
+		}, (err, success) => {
+			if(err){
+				reject(err);
+			} else {
+				resolve(success);
+			}
+		});
+    }
+  ], function (err, result) {
+    if(err){
+      return res.json({responsedata:{name:"error",status:0,message:err.message}});
+    }
+      return res.json({responsedata:{name:"success",status:1,message:result}});
+  });*/
+
+}
+
 
 exports.setting = (req, res, next) => {
 
