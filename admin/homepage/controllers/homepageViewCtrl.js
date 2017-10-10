baa4825888250686dc962e70a68ad027e66ca7f6
yash sharma <yash_sharma@seologistics.com>
2017-10-10 18:30:51 +0530

    Homepage Management finished

--- admin/homepage/controllers/homepageViewCtrl.js
+++ admin/homepage/controllers/homepageViewCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
-mimicTrading.controller('homepageViewCtrl', ['$scope', '$state', 'RestSvr', '$rootScope','appSvr','homepage',
-	($scope, $state, RestSvr, $rootScope, appSvr, homepage) => {
+mimicTrading.controller('homepageViewCtrl', ['$scope', '$state', 'RestSvr', '$rootScope','appSvr','homepage','$uibModal','homepageSvr',
+	($scope, $state, RestSvr, $rootScope, appSvr, homepage, $uibModal, homepageSvr) => {
 		
 		$scope.$on('$viewContentLoaded', () => {
 			/**
@@ -10,5 +10,30 @@ mimicTrading.controller('homepageViewCtrl', ['$scope', '$state', 'RestSvr', '$ro
 		}); 
 		$scope.homepage = homepage.record;
 		$scope.goToEdit = () => $state.go('homepage');
+
+		$scope.open = (action) => {
+			let modalInstance = $uibModal.open({
+				ariaLabelledBy: 'modal-title',
+				ariaDescribedBy: 'modal-body',
+				templateUrl: '/confirmationDialog/views/confirmation_dialog.html',
+				size: 'sm',
+				controller: 'confirmationDialogCtrl',
+				controllerAs: '$ctrl',
+				resolve: { data: () => action }
+			});
+
+		    modalInstance.result.then(selectedItem => {
+		    	if( selectedItem ) {
+		    		homepageSvr.removeBannerImage(btoa(selectedItem))
+		    		.then(response => $state.reload())
+		    		.catch(handleCatch);
+		    	}
+		      
+		    }, () => {
+		      console.log('Modal dismissed at: ' + new Date());
+		    });
+  		};
+
+  		let handleCatch = (errors) => App.alert({type: ('danger'), icon: ( 'warning'), message: errors.message, container: $rootScope.settings.errorContainer, place: 'prepend'});
 	}
 ]);
--- admin/homepage/factories/homepageSvr.js
+++ admin/homepage/factories/homepageSvr.js
@@ -2,6 +2,7 @@
 
 mimicTrading.factory('homepageSvr', ['RestSvr', (RestSvr) => {
     return {
-        getHomepageData: () => RestSvr.get(`homepage/view`)
+        getHomepageData: () => RestSvr.get(`homepage/view`),
+        removeBannerImage: (_id) => RestSvr.delete(`homepage/remove_banner/${_id}`)
     };
 }]);
