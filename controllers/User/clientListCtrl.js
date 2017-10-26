4af3b4308a990c69c0560a272df14b8f4ba63b94
Deepak Kr Yadav <yadav_deepak@seologistics.com>
2017-10-26 11:22:34 +0530

    changes in view client list api

--- controllers/User/clientListCtrl.js
+++ controllers/User/clientListCtrl.js
@@ -5,7 +5,10 @@ const
 	fs 			= require('fs'),
 	csv 		= require('csvtojson'),
 	json2csv    = require('json2csv'),
+	async		= require('async'),
+	mongoose	= require('mongoose'),
 	ClientList  = require(path.resolve('./models/ClientList')),
+	User        = require(path.resolve('./models/User')),
   	config 		= require(path.resolve(`./core/env/${process.env.NODE_ENV}`));
 
 exports.importClientList = (req, res, next) => {
@@ -15,9 +18,9 @@ exports.importClientList = (req, res, next) => {
 	csv()
 	.fromFile(csvFilePath)
 	.on('json',(jsonObj)=>{
-		jsonObj.user_id = req.body._id || "59d62896b93fbb37f6b0d270";
-		jsonObj.subscription_id = req.body.subscription_id || "270";
-		jsonObj.action = req.body.action || "signed up";
+		jsonObj.user_id = req.body._id;
+		jsonObj.subscription_id = req.body.subscription_id;
+		jsonObj.action = req.body.action;
 		jsonArr.push(jsonObj);
 	})
 	.on('done',(error)=>{
@@ -38,16 +41,64 @@ exports.importClientList = (req, res, next) => {
 };
 
 exports.clientList = (req,res,next) => {
-   	if( !req.body._id) {
+   	if( !req.body._id || !req.body.subscription_id ) {
 		return res.status(response.STATUS_CODE.UNPROCESSABLE_ENTITY)
-				  .json(response.required({message: 'Id is required'}));
+				  .json(response.required({message: 'Id and Subscription Id are required'}));
       }
-	ClientList.find({user_id:req.body._id},{__v:0,user_id:0},function (err, list) {
-		if( err ) {
-			return res.json(response.error(err));
-		}
-		res.json(response.success(list));
-	});
+
+    async.waterfall([
+    	function(done){
+    		User.aggregate([
+    		   {
+			      $match:{"_id": mongoose.Types.ObjectId( req.body._id) }
+			   },
+			   {
+			      $unwind: "$subscription_plan"
+			   },
+			   
+     	       {
+			   	  $match: { 'subscription_plan._id': mongoose.Types.ObjectId(req.body.subscription_id)} 
+			   },
+			   {
+			      $lookup: {
+			            from: "subscriptions",
+			            localField: "subscription_plan.plan_id",
+			            foreignField: "_id",
+			            as: "subscription_docs"
+			        }
+			   },
+			   { 
+			   	 $match: { "subscription_docs": { $ne: [] } } 
+			   },
+			   {  $unwind: "$subscription_docs"},
+			   {
+			   	   $project:{
+			   	   	    _id:0,
+			   	   		website_url: '$subscription_plan.website_url',
+			   	   		start_date: '$subscription_plan.start_date',
+			   	   		expiration_date: '$subscription_plan.expiration_date',
+			   	   		pla_name: '$subscription_docs.name',
+			   	   		plan_price:'$subscription_docs.price'
+			   		}
+			   	}
+    		],done)
+    	}
+    	,
+    	function(result,done){
+		    ClientList.find({subscription_id:mongoose.Types.ObjectId(req.body.subscription_id),user_id:req.body._id},{__v:0,user_id:0},function (err, list) {
+				if( err ) {
+					return res.json(response.error(err));
+				}
+				let fnlres={subscription_details:result[0],client_list:list};
+				done(null,fnlres);
+			});
+    	}
+
+    ],function(err,result){
+    	if(err) return next(err);
+		res.json(response.success(result));
+    })
+
 };
 
 
