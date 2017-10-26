f6e122be4d3eb375caecc3c8181b2992abe054fb
yash sharma <yash_sharma@seologistics.com>
2017-10-26 14:51:08 +0530

    WIP on script generation code

--- assets/js/socialproof.js
+++ assets/js/socialproof.js
@@ -7,7 +7,7 @@
     
     var template = [
     	'<script id="entry-template" type="text/x-handlebars-template">',
-	  		'<div class="lftInfoPopup active">',
+	  		'<div id="socialProofPopup" class="lftInfoPopup">',
 	  			'<div class="bubble">',
 	  				'<div class="bubbleImg">',
 	            		'<img alt="{{customer_name}}" src="{{image}}">',
@@ -19,15 +19,15 @@
 	    				    '<time><span>1 day ago</span></time>',
 	    				    '<div class="bubblePoweredBy">',
 	    				        '<span class="srOnly">Verified</span> ',
-	    				        '<i><img src="images/verifiedIcn.png" ></i> ',
+	    				        '<i><img src="{{verified}}" alt="verified"></i> ',
 	    				        '<span>by</span> ',
 	    				        '<a target="_blank" href="">SPT</a>',
 	    				    '</div>',
 	    				'</div>',
 	    			'</div>',
 	    			'<div class="close-button">',
-	            		'<a href="javascript:void(0)" class="clseBtnLftP">',
-	            			'<img src="images/closeIcn.png">',
+	            		'<a href="javascript:void(0)" id="closeButton" class="clseBtnLftP">',
+	            			'<img alt="close" src="{{closeIcon}}">',
 	            		'</a>',
 	        		'</div>',
 	    		'</div>',	
@@ -43,23 +43,52 @@
 
 	/* Check if handlebars script file is loaded or not */
 	var _Handlebars = function () {
-		setTimeout(function () {
-			timeout--;
-			if(typeof(window.Handlebars) != 'undefined'){
-				var template = Handlebars.compile(src);
-				var context = {customer_name: "David Robien", body: "This is my first post!"};
-				var html    = template(context);
-				document.getElementById("output").innerHTML += html;
-				var x = document.getElementById("output");
-				
-			} else if (timeout > 0) {
-				_Handlebars();
-			} else {
-				console.error('Failed to load script, check your network connection');
-			}
-		}, 100);
+		setTimeout(compileTemplate, 100);
 	}
 	_Handlebars();
-	
-	
+
+	function compileTemplate() {
+		timeout--;
+		if(typeof(window.Handlebars) != 'undefined'){
+			var template = Handlebars.compile(src);
+			var context = {
+				customer_name: "David Robien", 
+				closeIcon: "http://localhost:9000/assets/images/closeIcn.png",
+				image: "http://localhost:9000/assets/images/user_dp_bigl.png",
+				verified: "http://localhost:9000/assets/images/verifiedIcn.png"
+			};
+			var html    = template(context);
+			document.getElementById("output").innerHTML += html;
+			var x = document.getElementById("output");
+			
+			// show popup
+			showPop();
+
+			// hide popup
+			hidePop(); hidePopByCloseButton();
+		} else if (timeout > 0) {
+			_Handlebars();
+		} else {
+			console.error('Failed to load script, check your network connection');
+		}
+	}
+
+	function showPop() {
+		setTimeout(function () {
+			document.getElementById('socialProofPopup').className = "lftInfoPopup active";
+			setTimeout(function () {
+				hidePop();
+				showPop();
+			},2500);
+		},1000);
+	}
+
+	function hidePopByCloseButton() {
+		document.getElementById('closeButton').addEventListener('click', function () {
+			document.getElementById('socialProofPopup').className = "lftInfoPopup";
+		});	
+	}
+	function hidePop() {
+		document.getElementById('socialProofPopup').className = "lftInfoPopup";
+	}
 }());
