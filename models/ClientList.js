df498d2efc39f63ea73a097c72f7b7a173066962
yash sharma <yash_sharma@seologistics.com>
2017-10-27 12:45:26 +0530

    WIP on view client list

--- models/ClientList.js
+++ models/ClientList.js
@@ -4,30 +4,30 @@ const mongoose 	= require('mongoose'),
 Schema 			= mongoose.Schema,
 
 clientListSchema   = new Schema({
-		name: {
-			type: String
-		},
-		user_id:{
-			type: mongoose.Schema.Types.ObjectId
-		},
-		subscription_id:{
-			type:mongoose.Schema.Types.ObjectId
-		},
-		image_url:{
-			type: String
-	    },
-		location: {
-			type: String
-		},
-		plan: {
-			type: String
-		},
-		date: {
-			type: String
-		},
-		action:{
-			type:String
-		}
+	name: {
+		type: String
+	},
+	user_id:{
+		type: mongoose.Schema.Types.ObjectId
+	},
+	subscription_id:{
+		type: mongoose.Schema.Types.ObjectId
+	},
+	image_url:{
+		type: String
+    },
+	location: {
+		type: String
+	},
+	plan: {
+		type: String
+	},
+	date: {
+		type: String
+	},
+	action:{
+		type:String
+	}
 },{
     timestamps: {
         createdAt: 'created_at',
